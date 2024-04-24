27050f004ef158a814afcd6c6cce95d3006ed35b
DESKTOP-DMA1DR1\Hello <yw27263@westridge.org>
2024-04-24 08:55:41 -0700

    Adding template files

---
+++ fcse-internetproject2/script.js
@@ -0,0 +1,18 @@
+/* Cat facts API endpoint:
+ * https://cat-fact.herokuapp.com/facts
+ * 
+ * Cat images API endpoint:
+ * https://api.thecatapi.com/v1/images/search
+ */
+
+async function getCat() {
+  let response = await fetch("what goes here?");
+  if (response.ok) {
+    let json = await response.json();
+    console.log(json);
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
+}
+
+getCat();
